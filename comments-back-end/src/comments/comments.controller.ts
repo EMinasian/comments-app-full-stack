92ce23d82cb613d0fbbdb3a57d237eb8302d9883
erik.minasian <eminasian@webmd.net>
2026-02-07 17:20:01 +0400

    feat: initial put and delete routes set up

--- comments-back-end/src/comments/comments.controller.ts
+++ comments-back-end/src/comments/comments.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import comments from '../../mocks/comments.json';
+import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Controller('comments')
 export class CommentsController {
@@ -28,5 +40,23 @@ export class CommentsController {
       content: createCommentDto.content,
       authorId: createCommentDto.authorId,
     };
+  }
+
+  // Put /comments/:id
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
+    return {
+      id: id,
+      title: updateCommentDto.title,
+      content: updateCommentDto.content,
+      authorId: updateCommentDto.authorId,
+    };
+  }
+
+  // Delete /comments/:id
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    console.log('the id was', id); //TODO: replace with deletion logic
   }
 }
---
+++ comments-back-end/src/comments/dto/update-comment.dto.ts
@@ -0,0 +1,5 @@
+export class UpdateCommentDto {
+  title: string;
+  content: string;
+  authorId: string;
+}
