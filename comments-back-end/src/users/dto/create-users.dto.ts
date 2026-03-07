c5d20a52e56d240d0c4a53c38d8e56e890e21612
erik.minasian <eminasian@webmd.net>
2026-03-07 11:17:16 +0400

    fix: changes to user dto

--- comments-back-end/src/users/dto/create-users.dto.ts
+++ comments-back-end/src/users/dto/create-users.dto.ts
@@ -1,18 +1,20 @@
-import { IsString, Length } from 'class-validator';
+import { IsEmail, IsString, IsStrongPassword, Length } from 'class-validator';
 
 export class CreateUsersDto {
-  @IsString()
-  @Length(1, 255)
-  firstName: string;
-
-  @IsString()
-  @Length(1, 255)
-  lastName: string;
-
   @IsString()
   @Length(1, 255)
   username: string;
 
   @IsString()
   profilePictureUrl: string;
+
+  @IsString()
+  @Length(1, 255)
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Length(8, 255)
+  @IsStrongPassword()
+  password: string;
 }
