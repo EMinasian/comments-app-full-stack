81acf183ef2130d864c19704be6b04c5dd1f2337
erik.minasian <eminasian@webmd.net>
2026-03-07 11:45:17 +0400

    fix: pino logger

--- comments-back-end/src/app.module.ts
+++ comments-back-end/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommentsModule } from './comments/comments.module';
 import { UsersModule } from './users/users.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CommentsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    LoggerModule.forRoot(),
+    CommentsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
