6a2cda943a65390c67965d371cdd968a5d82684b
erik.minasian <eminasian@webmd.net>
2026-04-24 15:44:57 +0400

    feat: link products in list to their single product page

--- products-front-end/components/Products/Prodcuts.tsx
+++ products-front-end/components/Products/Prodcuts.tsx
@@ -2,6 +2,7 @@ import { Grid } from "@mui/material";
 import Product from "../Product/Product";
 import { getProducts } from "./actions";
 import type { ProductType } from "@/types/prodcuts";
+import Link from "next/link";
 
 const Products = async () => {
   const products = (await getProducts()) as ProductType[];
@@ -10,7 +11,9 @@ const Products = async () => {
     <Grid container spacing={3}>
       {products.map((product: ProductType) => (
         <Grid size={{ xs: 12, md: 6, lg: 4 }} key={product.id}>
-          <Product product={product} />
+          <Link href={`/products/${product.id}`} className="no-underline">
+            <Product product={product} />
+          </Link>
         </Grid>
       ))}
     </Grid>
